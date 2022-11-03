95137613453b9135feb2472f8dfabf7cb70dab25
Ardi Syahputra <ardisaputraa12@gmail.com>
2022-11-03 07:47:03 +0700

    Functions for handle error and success message

--- app/utils.py
+++ app/utils.py
@@ -77,11 +77,26 @@ def run_query(query, commit: bool = False):
             return [dict(row) for row in conn.execute(query)]
 
 def error_message(sts: int, msg: str):
+    """
+    This function is for handle error message.\n
+    sts => status code,\n
+    msg => message for return
+    """
     return {"error": msg}, sts
 
-def success_message(sts: int, data: str = None, msg: str = None, tkn: str = None, row: bool = False):
+def success_message(sts: int, data: str = None, msg: str = None, key: str = None, tkn: str = None, row: bool = False):
+    """
+    This function is for handle success message.\n
+    sts => status code,\n
+    data => result from run_query function,\n
+    msg => message for return,\n
+    key => define new key (default is data),\n
+    tkn => token or id,\n
+    row => show total row from parameter data if True (default is False)
+    """
     val = {"data": [data]}
     if tkn!=None: val["token"] = tkn
     if row!=False: val["total_rows"]= len(data)
     if msg!=None: return msg, sts
+    if key!=None: val = {f"{key}": data}
     return val, sts
