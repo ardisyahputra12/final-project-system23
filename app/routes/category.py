45f8f8d4c6d248d1e115e78784feb074aa25b355
Ardi Syahputra <ardisaputraa12@gmail.com>
2022-11-03 07:51:12 +0700

    V1, category. 1 of 4 endpoints

--- app/routes/category.py
+++ app/routes/category.py
@@ -14,7 +14,15 @@ categories_bp = Blueprint("categories", __name__, url_prefix="/categories")
 
 @categories_bp.route("", methods=["GET"])
 def get_category():
-    pass
+    # For testing only
+    run_query(f"DELETE FROM categories", True)
+    run_query(f"INSERT INTO categories VALUES ('cid1', 'baju', '{datetime.now()}', 'admin')", True)
+    run_query(f"INSERT INTO categories VALUES ('cid2', 'celana', '{datetime.now()}', 'admin')", True)
+    run_query(f"INSERT INTO categories VALUES ('cid3', 'kaos', '{datetime.now()}', 'admin')", True)
+    # http://127.0.0.1:5000/categories
+
+    query = run_query(f"SELECT category_id, category_name FROM categories")
+    return success_message(200, query)
 
 @categories_bp.route("", methods=["POST"])
 def create_category():
