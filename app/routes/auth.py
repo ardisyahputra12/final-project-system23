61a07d0be2ed46ac4b5a7c2702fecb9a8713e31e
Ardi Syahputra <ardisaputraa12@gmail.com>
2022-12-01 08:01:41 +0700

    V2, auth. Crosscheck and fix some bug

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,5 @@
 import uuid
+import re
 from flask import request
 from sqlalchemy import (
     update,
@@ -23,12 +24,59 @@ from . import sign_up_bp, sign_in_bp
 
 @sign_up_bp.route("", methods=["POST"])
 def sign_up():
-    pass
+    body = request.json
+    id = uuid.uuid4()
+    pattern_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    pass_hash = generate_password_hash(body["password"])
+
+    if ("name" not in body) or (body["name"] == ''):
+        return error_message(400, "name can't be empty")
+    elif ("email" not in body) or (body["email"] == ''):
+        return error_message(400, "email can't be empty")
+    elif ("phone_number" not in body) or (body["phone_number"] == ''):
+        return error_message(400, "phone_number can't be empty")
+    elif ("password" not in body) or (body["password"] == ''):
+        return error_message(400, "password can't be empty")
+    elif re.match(pattern_email, body["email"]) == False:
+        return error_message(400, "invalid email")
+    elif any(val.isnumeric() for val in body["phone_number"]) == False:
+        return error_message(400, "phone_number must fill with number")
+    elif len(body["password"]) < 8:
+        return error_message(400, "password must contain at least 8 characters")
+    elif len([val for val in body["password"] if val.islower()]) < 1:
+        return error_message(400, "Password must contain a lowercase letter")
+    elif len([val for val in body["password"] if val.isupper()]) < 1:
+        return error_message(400, "Password must contain an uppercase letter")
+    elif any(val.isnumeric() for val in body["password"]) == False:
+        return error_message(400, "Password must contain a number")
+    elif run_query(select(Users.email).where(Users.email==body["email"] and Users.password==pass_hash)):
+        return error_message(409, f"email '{body['email']}' already used")
+    else:
+        run_query(insert(Users).values(id=id, name=body["name"], email=body["email"], phone_number=body["phone_number"], password=pass_hash, create_at=format_datetime(), create_by=body["name"]), True)
+        return success_message(201, msg_a="success, user created")
+
 
 @sign_in_bp.route("", methods=["POST"])
 def sign_in():
-    # For example only
-    name = request.get_json()["name"]
-    user_id = run_query(select(Users.id).where(Users.name==name))[0]["id"]
-    token = encode_auth_token(user_id)
-    return success_message(200, data="Login success", key="message", tkn=token)
+    body = request.json
+
+    if ("email" not in body) or (body["email"] == ''):
+        return error_message(400, "email can't be empty")
+    if ("password" not in body) or (body["password"] == ''):
+        return error_message(400, "password can't be empty")
+
+    pass_hash = generate_password_hash(body["password"])
+    user = run_query(select(Users.email, Users.password).where(Users.email==body["email"] and Users.password==pass_hash))
+
+    try:
+        user[0]
+    except Exception:
+        return error_message(400, "email or password is wrong, your account not found")
+
+    if (run_query(select(Users.email).distinct().where(Users.email==body["email"] and Users.password==pass_hash)) != []) and (check_password_hash(user[0]['password'], body["password"]) == True):
+        query = run_query(select(Users.name, Users.email, Users.phone_number, Users.type).where(Users.email==body["email"] and Users.password==pass_hash))[0]
+        user_id = run_query(select(Users.id).where(Users.email==body["email"] and Users.password==pass_hash))[0]["id"]
+        token = encode_auth_token(user_id)
+        return success_message(200, data=query, key="user_information", msg_a="Login success", tkn=token)
+    else:
+        return error_message(401, "email or password is wrong")
