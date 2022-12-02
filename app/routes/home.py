f369a81340dc9e3343773ae87913b222c4415b0d
Ardi Syahputra <ardisaputraa12@gmail.com>
2022-12-03 05:37:27 +0700

    V2, home. Crosscheck and fix some bug

--- app/routes/home.py
+++ app/routes/home.py
@@ -7,7 +7,6 @@ from sqlalchemy import (
     select,
 )
 from app.utils.query import run_query
-from app.utils.format_datetime import format_datetime
 from app.utils.response import (
     error_message,
     success_message,
@@ -20,8 +19,14 @@ from . import home_bp
 
 @home_bp.route("/banner", methods=["GET"])
 def get_image():
-    pass
+    query = run_query(select(Products.id, Products.image, Products.title).where(Products.condition!="soft_delete").limit(5))
+    return success_message(200, data=query)
+
 
 @home_bp.route("/category", methods=["GET"])
 def get_category():
-    pass
+    query = run_query(select(Categories.id, Categories.title).where(Categories.status=="available"))
+    for i in query:
+        img = run_query(select(Products.image).where(Products.category_id==i["id"], Products.condition!="soft_delete").limit(1))
+        i["image"] = img[0]["image"]
+    return success_message(200, data=query)
