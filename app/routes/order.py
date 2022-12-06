3baf15595862570fbb94f2bd5efd31506d10cdd3
strcxs <rdf.faishal@gmail.com>
2022-12-06 03:02:29 +0000

    fix bug

--- app/routes/order.py
+++ app/routes/order.py
@@ -29,93 +29,57 @@ def create_order(current_user):
     address = body.get("shipping_address")
     user_name = run_query(select(Users.name).where(Users.id==current_user))[0]["name"]
     
-    for x in run_query(select(Carts.user_id, func.sum(Products.price*Carts.quantity)).filter(Carts.product_id==Products.id).where(Carts.user_id==current_user).group_by(Carts.user_id)):
+    for z in run_query(select(Carts.user_id, func.sum(Products.price*Carts.quantity)).filter(Carts.product_id==Products.id).where(Carts.user_id==current_user).group_by(Carts.user_id)):
         if method == "regular":
-            if x['sum_1'] < 200:
-                total = int(x['sum_1']*(15/100))
-                if total > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
-                    return error_message(401,"your balance is not enough")
-                else:
-                    result_balance= int(run_query(select(Users.balance).where(Users.id==current_user))[0]['balance'])-total
-                    # result_balance_admin= int(run_query(select(Users.balance).where(Users.is_admin==True))[0]['balance'])+total
-                    products_result=[]
-                    for x in run_query(select(Orders).where(Orders.user_id==current_user)):
-                        for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
-                            products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
-                    # run_query(insert(Orders).values(products=products_result),True)
-                        run_query(delete(Carts).where(Carts.user_id==current_user),True)
-                    run_query(insert(Orders).values(products= products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(total),create_at=format_datetime(),create_by=user_name),True)
-                    run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
-                    # run_query(update(Users).values(balance=result_balance_admin).where(Users.is_admin==True))
-                    return success_message(200,"order success")
-            elif x['sum_1'] >= 200:
-                total = int(x['sum_1']*(20/100))
-                if total > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
-                    return error_message(401,"your balance is not enough")
-                else:
-                    result_balance= int(run_query(select(Users.balance).where(Users.id==current_user))[0]['balance'])-total
-                    # result_balance_admin= int(run_query(select(Users.balance).where(Users.is_admin==True))[0]['balance'])+total
-                    products_result=[]
-                    for x in run_query(select(Orders)):
-                        for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
-                            products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
-                    # run_query(insert(Orders).values(products=products_result),True)
-                        run_query(delete(Carts).where(Carts.user_id==current_user),True)
-                    run_query(insert(Orders).values(products= products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(total),create_at=format_datetime(),create_by=user_name),True)
-                    run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
-                    # run_query(update(Users).values(balance=result_balance_admin).where(Users.is_admin==True))
-                    return success_message(200,"order success")
-        elif method == "next day":
-            if x['sum_1'] < 300:
-                total = int(x['sum_1']*(20/100))
-                if total > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
-                    return error_message(401,"your balance is not enough")
-                else:
-                    result_balance= int(run_query(select(Users.balance).where(Users.id==current_user))[0]['balance'])-total
-                    # result_balance_admin= int(run_query(select(Users.balance).where(Users.is_admin==True))[0]['balance'])+total
-                    # run_query(update(Carts).values(status='soft_delete'),True)
-                    products_result=[]
-                    for x in run_query(select(Orders)):
-                        for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
-                            products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
-                        run_query(delete(Carts).where(Carts.user_id==current_user),True)
-                    # run_query(insert(Orders).values(products=products_result),True)
-                    run_query(insert(Orders).values(products= products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(total),create_at=format_datetime(),create_by=user_name),True)
-                    run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
-                    # run_query(update(Users).values(balance=result_balance_admin).where(Users.is_admin==True))
-                    return success_message(200,"order success")
-            elif x['sum_1'] >= 300:
-                total = int(x['sum_1']*(25/10))
-                if total > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
-                    return error_message(401,"your balance is not enough")
-                else:
-                    result_balance= int(run_query(select(Users.balance).where(Users.id==current_user))[0]['balance'])-total
-                    # result_balance_admin= int(run_query(select(Users.balance).where(Users.is_admin==True))[0]['balance'])+total
-                    # run_query(update(Carts).values(status='soft_delete'),True)
-                    products_result=[]
-                    for x in run_query(select(Orders)):
-                        for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
-                            products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
-                        run_query(delete(Carts).where(Carts.user_id==current_user),True)
-                    # run_query(insert(Orders).values(products=products_result),True)
-                    run_query(insert(Orders).values(products= products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(total),create_at=format_datetime(),create_by=user_name),True)
-                    run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
-                    # run_query(update(Users).values(balance=result_balance_admin).where(Users.is_admin==True))
-                    return success_message(200,"order success")
+            products_in_cart = run_query(select(Carts.quantity, Products.price).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user))
+            total_price = sum(float(val["price"] * val["quantity"]) for val in products_in_cart)
+            regular_price = (15*total_price)/100 if total_price < 200 else (20*total_price)/100
             
+            price_all = regular_price+z['sum_1']
+            if price_all > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
+                return error_message(401,"your balance is not enough")
+            else:
+                products_result=[]
+                result_balance = run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']-price_all
+                for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
+                    products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
+                run_query(insert(Orders).values(products=products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(z['sum_1']),create_at=format_datetime(),create_by=user_name),True)
+                run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
+                run_query(delete(Carts).where(Carts.user_id==current_user),True)
+        
+        elif method == "next day":
+            products_in_cart = run_query(select(Carts.quantity, Products.price).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user))
+            total_price = sum(float(val["price"] * val["quantity"]) for val in products_in_cart)
+            next_day_price = (20*total_price)/100 if total_price < 300 else (25*total_price)/100
+        
+            price_all = next_day_price+x['sum_1']
+            if price_all > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
+                return error_message(401,"your balance is not enough")
+            else:
+                products_result=[]
+                result_balance = run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']-price_all
+                for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
+                    products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
+                run_query(insert(Orders).values(products=products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(z['sum_1']),create_at=format_datetime(),create_by=user_name),True)
+                run_query(update(Users).values(balance=result_balance).where(Users.id==current_user),True)
+                run_query(delete(Carts).where(Carts.user_id==current_user),True)
+                    
+    return success_message(200,"order success")
+
 @orders_bp.route("", methods=["GET"])
 @decode_auth_token
 def get_orders(current_user):
     data=[]
     for x in run_query(select(Users).where(Users.is_admin==False)):
+        total_all = 0
         for z in run_query(select(Orders).where(Orders.user_id==x['id']).group_by(Orders.id)):
             order_id = z['id']
             create_at = z['create_at']
-            # for c in run_query(select(Carts.user_id, func.sum(Products.price*Carts.quantity)).filter(Carts.product_id==Products.id).where(Carts.user_id==x['id']).group_by(Carts.user_id)):
-            #     total = c['sum_1']
+            total = 0
             for a in z['products']:
-                total =+ a['price']
-        data.append({"id":order_id,"user_name":x['name'],"create_at":create_at,"user_id":x['id'],"user_email":x['email'],"total": total})
+                total += int(a['price']*a['details']['quantity'])
+            total_all += total
+        data.append({"id":order_id,"user_name":x['name'],"create_at":create_at,"user_id":x['id'],"user_email":x['email'],"total": total_all})
     return success_message(200,data)
 
 @orders_bp.route("/del", methods=["GET"])
