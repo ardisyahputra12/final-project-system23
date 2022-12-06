204e543b46c8164ad6386db6d1a058bfc3847ffe
ardisyahputraa12 <ardisaputraa12@gmail.com>
2022-12-06 04:50:30 +0000

    Update minor in order

--- app/routes/order.py
+++ app/routes/order.py
@@ -28,17 +28,17 @@ def create_order(current_user):
     address = body.get("shipping_address")
     user_name = run_query(select(Users.name).where(Users.id==current_user))[0]["name"]
 
-    shipping_method = [{"name": "regular", "price": 0}, {"name": "next day", "price": 0}]
-    products_in_cart = run_query(select(Carts.quantity, Products.price).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user))
-    total_price = sum(float(val["price"] * val["quantity"]) for val in products_in_cart)
-
-    regular_price = (15*total_price)/100 if total_price < 200 else (20*total_price)/100
-    shipping_method[0]["price"] = int(regular_price)
-
-    next_day_price = (20*total_price)/100 if total_price < 300 else (25*total_price)/100
-    shipping_method[1]["price"] = int(next_day_price)
-
     def order(val):
+        shipping_method = [{"name": "regular", "price": 0}, {"name": "next day", "price": 0}]
+        products_in_cart = run_query(select(Carts.quantity, Products.price).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user))
+        total_price = sum(float(val["price"] * val["quantity"]) for val in products_in_cart)
+        
+        regular_price = (15*total_price)/100 if total_price < 200 else (20*total_price)/100
+        shipping_method[0]["price"] = int(regular_price + total_price)
+
+        next_day_price = (20*total_price)/100 if total_price < 300 else (25*total_price)/100
+        shipping_method[1]["price"] = int(next_day_price + total_price)
+
         if shipping_method[val]["price"] > run_query(select(Users.balance).where(Users.id==current_user))[0]['balance']:
             return error_message(401,"your balance is not enough")
         else:
@@ -47,7 +47,7 @@ def create_order(current_user):
                 for x in run_query(select(Carts,Products.image,Products.price,Products.title).filter(Products.id==Carts.product_id).where(Carts.user_id==current_user)):
                     products_result.append({"id":x['id'],"details":{"quantity":x["quantity"],"size":x["size"]},"price":x["price"],"image":x["image"],"name":x["title"]})
                 run_query(delete(Carts).where(Carts.user_id==current_user),True)
-            run_query(insert(Orders).values(products=products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=format(shipping_method[val]["price"]),create_at=format_datetime(),create_by=user_name),True)
+            run_query(insert(Orders).values(products=products_result,id=uuid.uuid4(),user_id=current_user,shipping_method=method,shipping_address=address,total_price=shipping_method[val]["price"],create_at=format_datetime(),create_by=user_name),True)
             run_query(update(Users).values(balance=Users.balance-shipping_method[val]["price"]).where(Users.id==current_user),True)
             run_query(update(Users).values(balance=Users.balance+shipping_method[val]["price"]).where(Users.is_admin==True), True)
             return success_message(200,"order success")
@@ -63,9 +63,5 @@ def get_orders(current_user):
     data=[]
     for x in run_query(select(Users).where(Users.is_admin==False)):
         for z in run_query(select(Orders).where(Orders.user_id==x['id']).group_by(Orders.id)):
-            order_id = z['id']
-            create_at = z['create_at']
-            for a in z['products']:
-                total = a['price']
-            data.append({"id":order_id,"user_name":x['name'],"created_at":create_at,"user_id":x['id'],"user_email":x['email'],"total": total})
+            data.append({"id":z['id'],"user_name":x['name'],"created_at":z['create_at'],"user_id":x['id'],"user_email":x['email'],"total": z['total_price']})
     return success_message(200,data)
